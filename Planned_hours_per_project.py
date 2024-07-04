775a6050619aa968b5ac629c6a98d821ced6da72
p_saboya <paola.saboya@gmail.com>
2024-07-03 21:12:15 -0500

    This modification accepts any number of assigned projects and any number of hours between 0 and the maximum number of work hours for the week

--- Planned_hours_per_project.py
+++ Planned_hours_per_project.py
@@ -27,96 +27,137 @@ def find_excel_projects_location() :
         print("Resource name is not in array")
     invalid_project_number = True
     while invalid_project_number == True :
-        number_total_assigned_projects = input("enter total of assigned projects ")
+        number_total_assigned_projects = input("enter total of assigned projects => ")
         if not number_total_assigned_projects.isnumeric() or number_total_assigned_projects == "0" :
             print('Invalid option') 
         else:
             invalid_project_number = False
     Excel_row_final_project = Excel_row_first_project + int(number_total_assigned_projects)
-    return Excel_row_first_project, Excel_row_final_project
+    return Excel_row_first_project, Excel_row_final_project, number_total_assigned_projects
     
-#Excel_row_first_project, Excel_row_final_project = find_excel_projects_location()
+Excel_row_first_project, Excel_row_final_project, number_total_assigned_projects = find_excel_projects_location()
 #print(type(Excel_row_first_project))
 
-#Function to choose the project weight (in hours) during the following week
-# 0 => No Started or On hold, 1 => Minimum effort, 2 => Average effort, 3 => Demanding ")
-# 0 = 0 hours per week, 1 = 2hours per week, 2 = 4 hours per week, 3 = 8 hours per week
-def receiving_weight_proj_this_week() :
-  options = ["0", "1", "2", "3"] 
-  #weight_proj_this_week = input("enter project weight (number between 0-3) : 0 => No Started or On hold, 1 => Minimum effort, 2 => Average effort, 3 => Demanding ")
-  #weight_proj_this_week = int(weight_proj_this_week)
-  invalid_weight = True
-  while invalid_weight == True:
-    weight_proj_this_week = input("Enter project weight (number between 0-3) : 0 => No Started or On hold, 1 => Minimum effort, 2 => Average effort, 3 => Demanding ")
-    if not weight_proj_this_week in options :
-        print('Invalid option')
-    else:
-        invalid_weight = False
-  return int(weight_proj_this_week)
-  
-#weight_proj_this_week = receiving_weight_proj_this_week()
-#print(weight_proj_this_week)
-#print(type(weight_proj_this_week))
-
-
-#Function --> It calculates the percentage of dedication according to priority and working hours of the week
-def percentage_calculation() : 
-    #This is to validate that the days of the week are between 1 and 5
+#Funtion --> to validate that the days of the week are between 1 and 5
+def receiving_number_working_days_converts_to_hours() :
     list_number_days_week = []
     for day in range(1, 6):
         list_number_days_week.append(str(day))
     #print(list_number_days_week)
     invalid_number_working_days = True
     while invalid_number_working_days == True :
-        number_working_days_this_week = input("enter working days this week ")
+        number_working_days_this_week = input("enter working days this week => ")
         if not number_working_days_this_week in list_number_days_week :
             print('Invalid option')
         else:
             invalid_number_working_days = False
-    number_working_hours_this_week = int(number_working_days_this_week)*8 # 8 is the number of working hours per day
-    Excel_row_first_project, Excel_row_final_project = find_excel_projects_location ()
-    list_percentage_per_project = [] #This is to add the total dedication percentages of all projects for a week
-    #print(Excel_row_first_project)
+    number_working_hours_this_week = int(number_working_days_this_week)*8 # 8 h is the number of working hours per day in Colombia
+    return number_working_hours_this_week
+    
+number_working_hours_this_week = receiving_number_working_days_converts_to_hours()
+
+'''
+#Funtion --> It calculates the working hours per week  
+def arragement_calculated_percentage_calculation_this_week(number_working_hours_this_week) :
+    for project in range(Excel_row_first_project, Excel_row_final_project, 1) :
+        percentage_calculation_this_week = (number_hours_this_week * 100) / number_working_hours_this_week
+        print("percentage_calculation_this_week => % ", percentage_calculation_this_week)
+        dataframe_pandas.loc[project, "Julio"] = percentage_calculation_this_week
+    return dataframe_pandas, percentage_calculation_this_week
+
+#dataframe_pandas, percentage_calculation_this_week = arragement_calculated_percentage_calculation_this_week(number_working_hours_this_week)'''
+
+#Function --> It calculates the percentage of dedication according to priority and working hours of the week
+def arragement_calculated_percentage_calculation_this_week(number_total_assigned_projects, number_working_hours_this_week, dataframe_pandas) : 
+    arregement_percentage_calculation_this_week_to_validate = []
     for project in range(Excel_row_first_project, Excel_row_final_project, 1) :
         project_name = dataframe_pandas.loc[project, 'Descripcion']
         print(project_name)
-        weight_proj_this_week = receiving_weight_proj_this_week()
-        print(weight_proj_this_week)
-        #print(type(weight_proj_this_week))
-        if weight_proj_this_week == 0 :
-            number_hours_this_week = 0
-            print("number_hours_this_week => ", number_hours_this_week, "h")
-        elif weight_proj_this_week == 1 :
-            number_hours_this_week = 2
-            print("number_hours_this_week => ", number_hours_this_week, "h")
-        elif weight_proj_this_week == 2 :
-            number_hours_this_week = 4
-            print("number_hours_this_week => ", number_hours_this_week, "h")
-        else:
-            number_hours_this_week = 8
-            print("number_hours_this_week => ", number_hours_this_week, "h")
-
-        percentage_calculation_this_week = (number_hours_this_week * 100) / number_working_hours_this_week
+        #While --> to choose the project weight (in hours) during the following week
+        invalid_weight = True
+        while invalid_weight == True:
+          hours_proj_this_week = input("enter number of hours per week estimated for each project => ")
+          if not hours_proj_this_week.isnumeric() or int(hours_proj_this_week) < 0 or int(hours_proj_this_week) > 40 :
+              print('Invalid option')
+          else:
+              invalid_weight = False
+              hours_proj_this_week = int(hours_proj_this_week)
+        percentage_calculation_this_week = (hours_proj_this_week * 100) / number_working_hours_this_week
         print("percentage_calculation_this_week => % ", percentage_calculation_this_week)
-        dataframe_pandas.loc[project, "Junio"] = percentage_calculation_this_week
-        
-        list_percentage_per_project.append(percentage_calculation_this_week)
-    print(list_percentage_per_project)
-    sum_of_percentage_projets_week = sum(list_percentage_per_project)
-    print(sum_of_percentage_projets_week)
-    return sum_of_percentage_projets_week
-    
-    #Percentage validation between 100 % and 150 %
-    #if sum_of_percentage < 100 or sum_of_percentage > 150
-    
+        arregement_percentage_calculation_this_week_to_validate.append(percentage_calculation_this_week)
+    print(arregement_percentage_calculation_this_week_to_validate)
+    sum_arregement_percentage_calculation_this_week_to_validate =  sum(arregement_percentage_calculation_this_week_to_validate)
+    print(sum_arregement_percentage_calculation_this_week_to_validate)
+    return arregement_percentage_calculation_this_week_to_validate
 
+arregement_percentage_calculation_this_week_to_validate = arragement_calculated_percentage_calculation_this_week(number_total_assigned_projects, number_working_hours_this_week, dataframe_pandas)
+
+'''
+Para Excel
+dataframe_pandas.loc[project, "Julio"] = percentage_calculation_this_week
+        dataframe_pandas_modified = dataframe_pandas
+    print(dataframe_pandas_modified['Julio'][Excel_row_first_project:Excel_row_final_project])
+    print(type(dataframe_pandas_modified['Julio'][Excel_row_first_project:Excel_row_final_project]))
+    #dataframe_pandas.to_excel('C:/Users/User/Documents/BPS Technology Solutions/Matriz Asignacion TD_v2.xlsx', index=True, header=True)'''
+
+'''
+#Funtion --> Percentage validation between 100 % and 150 %
+dataframe_pandas_modified_final = []
+def validation_percentage_total_week(dataframe_pandas_modified, Excel_row_first_project, Excel_row_final_project) :
+    sum_colum_of_percentage_projets_week = (dataframe_pandas_modified['Julio'][Excel_row_first_project:Excel_row_final_project]).sum()
+    sum_colum_of_percentage_projets_week =  int(sum_colum_of_percentage_projets_week)
+    print(sum_colum_of_percentage_projets_week)
+    print(type(sum_colum_of_percentage_projets_week))
+    while sum_colum_of_percentage_projets_week < 100 or sum_colum_of_percentage_projets_week > 150 :
+        for percentage in range(Excel_row_first_project, Excel_row_final_project, 1) :
+            if dataframe_pandas_modified.loc[percentage, "Julio"] != 0 :
+                project_name = dataframe_pandas.loc[percentage, 'Descripcion']
+                print(project_name)
+                #While --> to choose the project weight (in hours) during the following week
+                # 0 => No Started or On hold, 1 => Minimum effort, 2 => Average effort, 3 => Demanding ")
+                # 0 = 0 hours per week, 1 = 2hours per week, 2 = 4 hours per week, 3 = 8 hours per week
+                options = ["0", "1", "2", "3"] #Valid options for projects week weight
+                invalid_weight = True
+                while invalid_weight == True:
+                  weight_proj_this_week = input("Enter project weight (number between 0-3) : 0 => No Started or On hold, 1 => Minimum effort, 2 => Average effort, 3 => Demanding ")
+                  if not weight_proj_this_week in options :
+                      print('Invalid option')
+                  else:
+                      invalid_weight = False
+                #If --> to assign the percentage of dedication to each project according to its weight in hours per week
+                if weight_proj_this_week == "0" :
+                    number_hours_this_week = 0
+                    print("number_hours_this_week => ", number_hours_this_week, "h")
+                elif weight_proj_this_week == "1" :
+                    number_hours_this_week = 2
+                    print("number_hours_this_week => ", number_hours_this_week, "h")
+                elif weight_proj_this_week == "2" :
+                    number_hours_this_week = 4
+                    print("number_hours_this_week => ", number_hours_this_week, "h")
+                else:
+                    number_hours_this_week = 8
+                    print("number_hours_this_week => ", number_hours_this_week, "h") 
+                percentage_calculation_this_week = (number_hours_this_week * 100) / number_working_hours_this_week
+                print("percentage_calculation_this_week => % ", percentage_calculation_this_week)
+                dataframe_pandas.loc[percentage, "Julio"] = percentage_calculation_this_week
+                dataframe_pandas_modified_final[percentage] = dataframe_pandas.loc[percentage, "Julio"] 
+                #print(dataframe_pandas_modified['Julio'][Excel_row_first_project:Excel_row_final_project])
+                #print(type(dataframe_pandas_modified['Julio'][Excel_row_first_project:Excel_row_final_project]))
+            else :
+               dataframe_pandas_modified_final[percentage] = dataframe_pandas_modified[percentage]
+    return dataframe_pandas_modified_final
+   
+dataframe_pandas_modified_final = validation_percentage_total_week(dataframe_pandas_modified, Excel_row_first_project, Excel_row_final_project)
+print(dataframe_pandas_modified_final)
+print(type(dataframe_pandas_modified_final))
+'''
             #print(f"project number {project}")  
             #dataframe_pandas.loc[project, "Junio"] = percentage_calculation_this_week
     #print(dataframe_pandas['Febrero'])
     #dataframe_pandas.to_excel('C:/Users/User/Documents/BPS Technology Solutions/Matriz Asignacion TD_v2.xlsx', index=True, header=True)
     #return None, None
     
-sum_of_percentage_projets_week = percentage_calculation()
+#sum_of_percentage_projets_week = percentage_calculation()
 
 '''
 def main () :
