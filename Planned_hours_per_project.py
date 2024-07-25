45183c8fd1297b500e916ceec163f3c31deecf0c
p_saboya <paola.saboya@gmail.com>
2024-07-24 21:23:37 -0500

    This allows you to manually decide if you agree or not with the total sum of the percentages

--- Planned_hours_per_project.py
+++ Planned_hours_per_project.py
@@ -2,25 +2,56 @@
 import pandas as pd
 import openpyxl
 import pylab
+import calendar
+import locale
+locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8') #Spahinsh configuration for the name of the months
 
-workbook_assignament_matrix = 'C:/Users/User/Documents/BPS Technology Solutions/Matriz Asignacion TD_v1.xlsx' # Import the Excel archive
-dataframe_pandas = pd.read_excel(workbook_assignament_matrix) # convert archive in dataframe - Pandas
+excel_assignament_matrix = 'C:/Users/User/Documents/BPS Technology Solutions/Matriz Asignacion TD_v1.xlsx' # Import the Excel archive
+dataframe_pandas = pd.read_excel(excel_assignament_matrix) # convert archive in dataframe - Pandas
+#assinement_sheet = dataframe_pandas['Asignacion']
 #print(dataframe_pandas)
 #print(dataframe_pandas['Recurso'])
 #valor_celda_persona = dataframe_pandas.loc[166, 'Recurso']
 #print(valor_celda_persona)
 
+'''
+#Function --> Search month and week to put the assinated percentage
+def find_month_week_location() :
+    print("dentro de find month")
+    list_months = calendar.month_name[1:]
+    print(list_months)
+    invalid_month = True
+    while invalid_month == True :
+        searched_month = input("enter the month of the year that you want to modify => ")
+        if not searched_month in list_months :
+            print('Invalid option')
+        else:
+            invalid_month = False
+    print(searched_month)
+    title_row = 1
+    firts_row = dataframe_pandas[title_row]
+    print(firts_row)
+    for month in dataframe_pandas[title_row] :
+        valor_celda = month.value
+        if valor_celda == searched_month :
+            print(f"Dato encontrado en la columna: {month.column}")
+            break'''
+    #return searched_month
+
+#searched_month = find_month_week_location()
+
+
 #Function --> Search resource's name to find the Excel row of the first and the last projects assigned to he/she
 #The result is de indexes of the Excel row first project and Excel row final project, 
 #Excel row final project is calculate with the sume of total of projects assigned to the person
 def find_excel_projects_location() :
+    #searched_name = input("enter the talent name that you want to find => ")
     searched_name = "Paola Saboyá"
-    #search_name = input("enter resource's name ")
     index = 1
     for index, name in dataframe_pandas['Recurso'].items() : 
         #print(name)
         if name == searched_name :
-           #print(f"The resource name is in the row {index}")
+           print(f"The resource name is in the row {index}")
            Excel_row_first_project = index
            break
     else :
@@ -61,10 +92,13 @@ number_working_hours_this_week = receiving_number_working_days_converts_to_hours
 def arragement_calculated_percentage_calculation_this_week(number_total_assigned_projects, number_working_hours_this_week, dataframe_pandas) : 
     cumulative_of_hours = []
     arregement_percentage_calculation_this_week = []
-    #arregement_percentage_calculation_this_week_str = []
-    sum_arregement_percentage_calculation_this_week = 0
+    arregement_percentage_calculation_this_week_str = []
+    sum_arregement_percentage_calculation_this_week = 0 
     #While --> This makes the minimum of 100% dedication estimated for the week
-    while sum_arregement_percentage_calculation_this_week < 100 :
+    manual_approval = False
+    while manual_approval == False :
+        print("first manual_approval => ", manual_approval)
+    #while sum_arregement_percentage_calculation_this_week < 100 :
         for project in range(Excel_row_first_project, Excel_row_final_project, 1) :
             project_name = dataframe_pandas.loc[project, 'Descripcion']
             print(project_name)
@@ -81,15 +115,24 @@ def arragement_calculated_percentage_calculation_this_week(number_total_assigned
             cumulative_sum_of_hours = sum(cumulative_of_hours)
             print("cumulative sum of hours => ", cumulative_sum_of_hours)
             percentage_calculation_this_week = (hours_proj_this_week * 100) / number_working_hours_this_week
-            print("percentage_calculation_this_week => % ", percentage_calculation_this_week)
+            percentage_calculation_this_week_round = round(percentage_calculation_this_week, 3)
+            print("percentage_calculation_this_week => % ", percentage_calculation_this_week_round)
             arregement_percentage_calculation_this_week.append(percentage_calculation_this_week)
-            #arregement_percentage_calculation_this_week_str.append(str(percentage_calculation_this_week) + "%")
-            #Using Map function 
-        arregement_percentage_calculation_this_week_str = list(map(lambda project:str(percentage_calculation_this_week) + "%", arregement_percentage_calculation_this_week))   
-        print("arregement_percentage_calculation_this_week ",arregement_percentage_calculation_this_week)
+            arregement_percentage_calculation_this_week_str.append(str(percentage_calculation_this_week) + " %")
+        #Using Map function 
+        #arregement_percentage_calculation_this_week_str = list(map(lambda project:str(percentage_calculation_this_week) + "%", arregement_percentage_calculation_this_week))   
+        #print("arregement_percentage_calculation_this_week ",arregement_percentage_calculation_this_week)
         print("arregement_percentage_calculation_this_week_str ",arregement_percentage_calculation_this_week_str)
         sum_arregement_percentage_calculation_this_week = sum(arregement_percentage_calculation_this_week)
         print("sum_arregement_percentage_calculation_this_week => % ", sum_arregement_percentage_calculation_this_week)
+        manual_approval = input("Do you agree with the accumulated sum for the week of the assigned percentages for the projects?, If your answer is yes, please write Y if not, please write N => " )
+        manual_approval = manual_approval.lower()
+        if manual_approval == "y" : 
+            manual_approval = True
+        else :
+            manual_approval = False
+        #print("final manual_approval", manual_approval)
+        #print(type(manual_approval))
     return arregement_percentage_calculation_this_week_str
 
 arregement_percentage_calculation_this_week_str = arragement_calculated_percentage_calculation_this_week(number_total_assigned_projects, number_working_hours_this_week, dataframe_pandas)
@@ -97,7 +140,7 @@ arregement_percentage_calculation_this_week_str = arragement_calculated_percenta
 #Funtion --> This modifies the Excel file, writes the arregement_percentage_calculation_this_week on the column of the chose month 
 def modifinding_dataframe_pandas(dataframe_pandas, arregement_percentage_calculation_this_week_str, Excel_row_first_project, Excel_row_final_project) :
     range_Excel_rows_to_modify = range(Excel_row_first_project, Excel_row_final_project)
-    dataframe_pandas.loc[range_Excel_rows_to_modify, 'Julio'] = arregement_percentage_calculation_this_week_str
+    dataframe_pandas.loc[range_Excel_rows_to_modify, 'Mayo'] = arregement_percentage_calculation_this_week_str
     dataframe_pandas.to_excel('C:/Users/User/Documents/BPS Technology Solutions/Matriz Asignacion TD_v2.xlsx', index=True, header=True)
     #dataframe_pandas.to_excel('C:/Users/User/Documents/BPS Technology Solutions/Matriz Asignacion TD_v2.xlsx', index=True,  style=lambda x: x.apply(lambda v: v.rjust(len(v), '.') + '%'))
     return None, None
@@ -108,6 +151,3 @@ def main() :
     
 if __name__ == "__main__" :
     main()
-
-# Guardar el DataFrame en Excel con formato personalizado
-#df.to_excel("archivo.xlsx", index=False, style=lambda x: x.astype(object).apply(lambda v: v.strftime(formato_personalizado)))
